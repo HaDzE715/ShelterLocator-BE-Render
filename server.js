c94faa1c0650bbdad67225ac335ec54b5c0a5f69
Hade Bayaa <93552913+HaDzE715@users.noreply.github.com>
2024-02-20 17:17:29 +0200

    Uploaded Files
    Added cors to enable several requests.

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 8080;
+const sheltersRouter = require('./routes/sheltersRoute');
+const cors = require('cors');
+
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const url = process.env.DATABASE_URL.replace('{{DATABASE_USERNAME}}', username).replace('{{DATABASE_PASSWORD}}', password);
+
+mongoose.connect(url)
+.then(() =>{
+    console.log('Connected to the Database.');
+})
+.catch((error) => {
+    console.error('Error connecting to the MongoDB:', error);
+})
+
+app.use(cors());
+app.use(express.json());
+app.use('/shelters', sheltersRouter);
+
+app.listen(port, () => console.log(`Server has started listening on port ${port}`));
+
+module.exports = app;
